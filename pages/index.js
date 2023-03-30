64e3664ef094bce9a0d5325792470c6f2f53cad6
Amirreza <amirreza.m13825@gmail.com>
2023-03-30 19:05:12 +0430

    sort section added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
-import { ChevronDownIcon } from "@heroicons/react/24/outline";
+import {
+  AdjustmentsVerticalIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -6,7 +9,7 @@ export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2 min-h-screen">
+    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-[60px_minmax(300px,1fr)] min-h-screen">
       <div className="hidden md:block md:row-span-2 md:col-span-3">
         <div className="rounded-lg overflow-hidden">
           {/* accordion header*/}
@@ -29,27 +32,43 @@ export default function Home() {
           >
             <Link
               href={"#"}
-              className="block pr-4 py-2 mb-1 transition-all hover:bg-hover-color"
+              className="block pr-4 py-2 mb-1 transition-all hover:bg-hover-secondary-color"
             >
               همه مقالات
             </Link>
             <Link
               href={"#"}
-              className="block pr-4 py-2 mb-1 transition-all hover:bg-hover-color"
+              className="block pr-4 py-2 mb-1 transition-all hover:bg-hover-secondary-color"
             >
               فرانت‌اند
             </Link>
             <Link
               href={"#"}
-              className="block pr-4 py-2 transition-all hover:bg-hover-color"
+              className="block pr-4 py-2 transition-all hover:bg-hover-secondary-color"
             >
               بک‌اند
             </Link>
           </div>
         </div>
       </div>
-      <div className="bg-primary-color hidden md:block md:col-span-9">
-        Sort Desktop
+      <div className="hidden md:block md:col-span-9">
+        <div className="bg-primary-color rounded-lg px-4 py-1 flex items-center">
+          <div className="flex gap-x-2 items-center">
+            <AdjustmentsVerticalIcon className="w-6 h-6" />
+            <span>مرتب سازی:</span>
+          </div>
+          <ul className="flex items-center gap-x-4 mr-4">
+            <li className="p-2 rounded hover:bg-hover-primary-color transition-all cursor-pointer">
+              پر‌بازدید ترین
+            </li>
+            <li className="p-2 rounded hover:bg-hover-primary-color transition-all cursor-pointer">
+              محبوب ترین
+            </li>
+            <li className="p-2 rounded hover:bg-hover-primary-color transition-all cursor-pointer">
+              جدید ترین
+            </li>
+          </ul>
+        </div>
       </div>
       <div className="bg-primary-color md:col-span-9">Blogs</div>
     </div>
