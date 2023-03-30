c656ababad1297fddb86e6b6c09bf948d988f1ae
Amirreza <amirreza.m13825@gmail.com>
2023-03-30 17:30:17 +0430

    css variables (primary & secondary colors) added

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2 bg-gray-100 min-h-screen">
+    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2 min-h-screen">
       <div className="hidden md:block md:row-span-2 md:col-span-3">
-        <div className="bg-white rounded-lg overflow-hidden">
+        <div className="rounded-lg overflow-hidden">
           {/* accordion header*/}
           <div
-            className="flex justify-between items-center p-4 cursor-pointer bg-slate-200"
+            className="flex justify-between items-center p-3 cursor-pointer bg-primary-color"
             onClick={() => setIsOpen(!isOpen)}
           >
             <span>دسته‌بندی مقالات</span>
@@ -23,35 +23,35 @@ export default function Home() {
           </div>
           {/* accordion content */}
           <div
-            className={`overflow-hidden transition-[max-height] duration-[400ms] ${
+            className={`bg-secondary-color overflow-hidden transition-[max-height] duration-[400ms] ${
               isOpen ? "max-h-40" : "max-h-0"
             }`}
           >
             <Link
               href={"#"}
-              className="block pr-4 py-2 mb-1 transition-all hover:bg-slate-100"
+              className="block pr-4 py-2 mb-1 transition-all hover:bg-hover-color"
             >
               همه مقالات
             </Link>
             <Link
               href={"#"}
-              className="block pr-4 py-2 mb-1 transition-all hover:bg-slate-100"
+              className="block pr-4 py-2 mb-1 transition-all hover:bg-hover-color"
             >
               فرانت‌اند
             </Link>
             <Link
               href={"#"}
-              className="block pr-4 py-2 transition-all hover:bg-slate-100"
+              className="block pr-4 py-2 transition-all hover:bg-hover-color"
             >
               بک‌اند
             </Link>
           </div>
         </div>
       </div>
-      <div className="bg-blue-200 hidden md:block md:col-span-9">
+      <div className="bg-primary-color hidden md:block md:col-span-9">
         Sort Desktop
       </div>
-      <div className="bg-blue-200 md:col-span-9">سلام55</div>
+      <div className="bg-primary-color md:col-span-9">Blogs</div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "var(--primary-color)",
+        "secondary-color": "var(--secondary-color)",
+        "hover-color": "var(--hover-color)",
+      },
+    },
   },
   plugins: [],
 };
