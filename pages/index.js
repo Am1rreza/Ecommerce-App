83773689b60263cebea65fb751df1dbe586be09a
Amirreza <amirreza.m13825@gmail.com>
2023-03-30 16:42:51 +0430

    the structure of the home page created

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-import styles from "@/styles/Home.module.css";
-
 export default function Home() {
-  return <></>;
+  return (
+    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2 bg-gray-100 min-h-screen">
+      <div className="bg-blue-200 hidden md:block md:row-span-2 md:col-span-3">
+        Accordion
+      </div>
+      <div className="bg-blue-200 hidden md:block md:col-span-9">
+        Sort Desktop
+      </div>
+      <div className="bg-blue-200 md:col-span-9">سلام55</div>
+    </div>
+  );
 }
