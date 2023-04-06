21193af6892fae9aeab108ffea7b4fa67828815c
Amirreza <amirreza.m13825@gmail.com>
2023-04-07 01:42:58 +0430

    tailwind typography plugin installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,5 +15,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
 };
